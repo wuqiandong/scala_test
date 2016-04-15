5f1860b01ec533717831b27ae2dc75c2b180dee8
wuqiandong <wuqiandong1988@163.com>
2016-04-15 13:51:46 +0800

    1111

--- scala/src/main/scala/com/baofoo/scala/HelloWorld.scala
+++ scala/src/main/scala/com/baofoo/scala/HelloWorld.scala
@@ -2,6 +2,9 @@ package com.baofoo.scala
 
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    println("你好")
+    /*变量声明*/
+    var x = 5  //声明变量
+    var z:Double = 7  //类型可省略， Scala可以推断出数据类型
+    val y = 6  //声明常量
   }
 }
